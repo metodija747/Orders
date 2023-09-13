a2697b521a805b09eb8223e7cb6a476b1fdd911e
Metodija747 <metodijabucevski@yahoo.com>
2023-09-13 23:16:52 +0200

    Let's test it

--- src/main/java/OrdersApplication.java
+++ src/main/java/OrdersApplication.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 
 @ApplicationPath("/")
 @RegisterService
-@OpenAPIDefinition(info = @Info(title = "Orders Catalog API", version = "1.0.0"))
+@OpenAPIDefinition(info = @Info(title = "Orders  Catalog API", version = "1.0.0"))
 public class OrdersApplication extends Application {
     private static final Logger LOG = Logger.getLogger(OrdersApplication.class.getName());
     public OrdersApplication() {
